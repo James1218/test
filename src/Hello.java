238e9a16182b245be988bab74b83c91199871b33
James1218 <jamesyang1218@gmail.com>
2017-05-31 10:15:34 -0400

    add comment to test jenkins SCM auto pull

--- src/Hello.java
+++ src/Hello.java
@@ -2,6 +2,7 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//add comment to test jenkins
 		System.out.println("This is Hello.java");
 	}
 
